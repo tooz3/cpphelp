be8df52f4a7b1b9cc34f3f233fb1091493763998
tooz3 <somet123@gir0fa.com>
2025-02-26 15:20:02 -0600

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,10 @@
-
+//header guard
+#indef gaurd 
+#define guard
+//contents that are being defined
+#define print std::cout <<
+#define input std::cin >>
+#define wait system("pause");
+#define newline std::cout << " \n ";
+#define duf return 0;
+#endef
